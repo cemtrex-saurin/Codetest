dce2d651c12440ddc129c3506533436631463bb4
unknown <sshah@cxr.agency>
2023-01-23 21:08:31 +0530

    rra

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ubuntu/nodejs-aws-codedeploy-pipeline/deploy.log
 
-echo 'pm2 restart nodejs-aws-codedeploy-pipeline' >> /home/ubuntu/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 restart nodejs-aws-codedeploy-pipeline >> /home/ubuntu/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'pm2 start npm -- start ' >> /home/ubuntu/nodejs-aws-codedeploy-pipeline/deploy.log
+pm2 start npm -- start >> /home/ubuntu/nodejs-aws-codedeploy-pipeline/deploy.log
